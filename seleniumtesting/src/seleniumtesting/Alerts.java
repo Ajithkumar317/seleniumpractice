6e013b2bdba64f209b564bb62e50ac8e5a1483d3
User <User@DESKTOP-TVJIMI8>
2021-11-08 11:02:33 +0530

    initial commit

--- seleniumtesting/src/seleniumtesting/Alerts.java
+++ seleniumtesting/src/seleniumtesting/Alerts.java
@@ -1,39 +1,47 @@
 package seleniumtesting;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.server.handler.GetAlertText;
 
 public class Alerts {
 
-	public static void main(String[] args) throws Throwable {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\eclipse-workspace\\seleniumtesting\\Driver\\chromedriver.exe");
+	public static void main(String[] args)  {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\git\\repository\\seleniumtesting\\Driver\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("http://demo.automationtesting.in/Alerts.html");
+		driver.manage().timeouts().implicitlyWait(60,TimeUnit.MILLISECONDS);
 		WebElement simple = driver.findElement(By.xpath("//button[contains(text(),'alert box:')]"));
 		simple.click();
-		Thread.sleep(3000);
-		driver.switchTo().alert().accept();
+		driver.switchTo().alert().dismiss();
 		WebElement confirm = driver.findElement(By.xpath("//a[contains(text(),'Alert with OK & Cancel')]"));
 
 		confirm.click();
 		WebElement confirmm = driver.findElement(By.xpath("//button[contains(text(),'click the button to display a confirm box')]"));
 
 		confirmm.click();
-		Thread.sleep(3000);
 
 		driver.switchTo().alert().dismiss();
 		WebElement prompt = driver.findElement(By.xpath("//a[contains(text(),'Alert with Textbox')]"));
+		
 
 		prompt.click();
+
 		WebElement promptt = driver.findElement(By.xpath("//button[contains(text(),'click the button to demonstrate the prompt box')]"));
 
 		promptt.click();
-		Thread.sleep(3000);
-		driver.switchTo().alert().sendKeys("Automation Demo site");
+	driver.switchTo().alert().sendKeys("Automation Demo site");
+		String text = driver.switchTo().alert().getText();
+
+
+
 		driver.switchTo().alert().accept();
-		driver.quit();
+		System.out.println( text);
+
 
 
 
